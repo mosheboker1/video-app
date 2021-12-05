329d7a8396aa9da41a2fb50fe872a212e849ac8e
Moshe Hay Boker <mosheboker1@gmail.com>
2021-12-05 14:07:52 +0200

    Fix permissions issues

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -2,19 +2,18 @@ import {Modal} from 'antd';
 import {ElementRef} from 'react';
 
 export interface MsgDialogProps {
-    title: string;
-    msg: string | ElementRef<any>;
+    data: {    title: string;    msg: string | ElementRef<any>;}
     showModal: boolean;
     handleOk?: () => void;
     handleCancel?: () => void;
 }
 
 export const MsgDialog = (props: MsgDialogProps) => {
-    return (<Modal title="Basic Modal"
+    return (<Modal title={props.data.title}
             visible={props.showModal}
             onOk={props.handleOk ?? (() => {
                 return null;
             })}>
-        {typeof props.msg === 'string' ? (<p>props.msg</p>) : props.msg}
+        {typeof props.data.msg === 'string' ? (<p>{props.data.msg}</p>) : props.data.msg}
     </Modal>);
 };
--- src/pages/recording/recording.page.tsx
+++ src/pages/recording/recording.page.tsx
@@ -1,59 +1,87 @@
 import VideoRecorder from 'react-video-recorder';
 import {MsgDialog} from '../../components/dialog';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {Video} from '../../models/video';
 import uploads from '../../services/files.service';
 import './recording.page.css';
 import dayjs from 'dayjs';
 
 export const RecordingPage = () => {
-    const [dialogMsg, setDialogMsg] = useState(null);
+    const [dialogData, setDialogData] = useState(null);
     const [startPos, setStartPos] = useState(null);
+    const [startCamera, setStartCamera] = useState(false);
 
-    if (!navigator.geolocation) {
-        // Todo: test it
-        setDialogMsg('You need to enable location');
-    }
+    useEffect(() => {
+        if (!navigator.geolocation) {
+            setDialogData({title: 'error', msg: 'You need to enable location'});
+        } else {
+            if (!localStorage.getItem('allowedCamera') ||
+                !localStorage.getItem('allowedLocation'))
+                setDialogData({title: 'Permissions', msg: 'Please enable your camera & location.'});
+            else
+                setStartCamera(true);
+        }
+    }, []);
+
 
     let recordStarted = () => {
-        navigator.geolocation.getCurrentPosition((e) => setStartPos( {lat: e.coords.latitude, lon: e.coords.longitude}));
+        navigator.geolocation.getCurrentPosition((e) => setStartPos({lat: e.coords.latitude, lon: e.coords.longitude}));
     };
 
     let recordCompleted = (blob) => {
         const video: Video = {
             createTime: dayjs().format('DD/MM/YYYY HH:mm'),
             blob: blob,
-            location:startPos
+            location: startPos
         };
         console.log({video});
         uploads.saveFile(video);
     };
 
-    const resetDialog = () => setDialogMsg(null);
+    let activate = () => {
+        closeDialog();
+        navigator.geolocation.getCurrentPosition(() => {
+            localStorage.setItem('allowedLocation', '1');
+        });
+        let constraints = {audio: true, video: true};
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                localStorage.setItem('allowedCamera', '1');
+                setStartCamera(true);
+            })
+            .catch((err) => {
+                if (err.name === 'NotAllowedError') {
+                    console.log('User has denied accessed');
+                }
+            });
+
+
+    };
+
+    const closeDialog = () => setDialogData(null);
     const showCameraError = (cameraE) => {
         console.log({cameraE});
-        setDialogMsg('Access to camera is block');
+        setDialogData('Access to camera is block');
     };
 
     return (
         <div className={'full'}>
-            <VideoRecorder
-                isOnInitially
+            {startCamera && <VideoRecorder
+                isOnInitially={startCamera}
                 isFlipped={true}
                 countdownTime={0}
                 onStartRecording={recordStarted}
                 onRecordingComplete={recordCompleted}
                 onError={showCameraError}
-                constraints={{ audio: true, video: { facingMode: { ideal: "environment" } } }}
-            />
-            <a className={'gallery-btn'} href="/gallery" >Gallery</a>
+                constraints={{audio: true, video: {facingMode: {ideal: 'environment'}}}}
+            />}
+            <a className={'gallery-btn'} href="/gallery">Gallery</a>
             {/*<button className={'switch-btn'} onClick={switchCamera}>Switch Camera</button>*/}
-            {dialogMsg &&
-            <MsgDialog title={'Error'}
-                       msg={dialogMsg}
-                       showModal={!!dialogMsg}
-                       handleCancel={resetDialog}
-                       handleOk={resetDialog}
+            {!!dialogData &&
+            <MsgDialog data={dialogData}
+                       showModal={!!dialogData}
+                       handleOk={activate}
+                       handleCancel={closeDialog}
             />
             }
         </div>
